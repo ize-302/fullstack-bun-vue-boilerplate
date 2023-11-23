91edb7c5939189415d7bf1c2c27bbb7736217bb1
ize-302 <adavizeozorku@gmail.com>
2023-11-23 21:02:04 +0100

    :sparkles: Update response

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 
-const app = new Elysia().use(cors()).get("/", () => "Hello Elysia!");
+const app = new Elysia().use(cors()).get("/", () => "Hello Elysia!!");
 
 app.listen(process.env.PORT ?? 8080);
 
